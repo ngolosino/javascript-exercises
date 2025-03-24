1630645cc88cd0f5f5e5f0d5463ac275f7ebb154
Nedrey Jon M. Golosino <ngolosino15@gmail.com>
2025-03-24 19:36:08 +0800

    repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
 
+    for(i = 0; i < num; i++ )
+        result += string;
+
+    return result;
 };
 
 // Do not edit below this line
