62b6b498a88d8cf3a6d26689b5c6fd41e0797d4d
Nedrey Jon M. Golosino <ngolosino15@gmail.com>
2025-04-01 21:48:48 +0800

    solved palindromes and fibonnaci problems

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    const reverseString = string.toLowerCase().split('').filter((character) => alphanumerical.includes(character)).reverse().join('');
 
+    const compareString = reverseString.split('').reverse().join('');
+
+    return compareString === reverseString;
 };
 
 // Do not edit below this line
